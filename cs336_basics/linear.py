187d7925ddb383b4b86dda22b48635ec367132fc
xzhy324 <xzhy324@qq.com>
2026-06-18 09:45:20 +0800

    implement Linear class

--- cs336_basics/linear.py
+++ cs336_basics/linear.py
@@ -1,9 +1,13 @@
 import torch
 from torch import Tensor
 from jaxtyping import Float
+import einops
 
 
 class Linear(torch.nn.Module):
+    """A fully connected linear layer."""
+    weight: Float[Tensor, "out_features in_features"]
+
     def __init__(self, in_features, out_features, device=None, dtype=None):
         """Construct a linear transformation module.
         Args:
@@ -12,7 +16,17 @@ class Linear(torch.nn.Module):
             device: torch.device | None = None Device to store the parameters on
             dtype: torch.dtype | None = None Data type of the parameters
         """
-        raise NotImplementedError("You need to implement this function.")
+        super().__init__()
+        self.weight = torch.nn.Parameter(
+            torch.empty((out_features, in_features), device=device, dtype=dtype)
+        )
+        # initialize the weights using truncated normal distribution
+        # 𝒩︀(𝜇 = 0, 𝜎2 = 2/(𝑑in+𝑑out) ) truncated at [−3𝜎, 3𝜎].
+        variance = 2 / (in_features + out_features)
+        std = variance**0.5
+        torch.nn.init.trunc_normal_(
+            self.weight, mean=0.0, std=std, a=-3 * std, b=3 * std
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Apply the linear transformation to the input.
@@ -21,8 +35,10 @@ class Linear(torch.nn.Module):
         Returns:
             torch.Tensor Output tensor of shape (batch_size, out_features)
         """
-        raise NotImplementedError("You need to implement this function.")
+        return einops.einsum(x, self.weight, "... d_in, d_out d_in -> ... d_out")
 
+
+# glue code for `uv run pytest -k test_linear`
 def run_linear(
     d_in: int,
     d_out: int,
@@ -41,4 +57,8 @@ def run_linear(
     Returns:
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
-    raise NotImplementedError("You need to implement this function.")
+    w = Linear(d_in, d_out)
+    # use use Module.load_state_dict to load the weights into the linear module
+    w.load_state_dict({"weight": weights})
+    y = w.forward(in_features)
+    return y
